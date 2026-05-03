56ea85c6bf7ada799ab569140bc893e73b8e147c
Kenny Garcia <kennygarcia15@yahoo.com>
2026-05-03 14:16:28 -0700

    Initial Commit

---
+++ cpsc 411 project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cpsc 411 project
+//
+//  Created by csuftitan on 5/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cpsc 411 project/cpsc_411_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  cpsc_411_projectApp.swift
+//  cpsc 411 project
+//
+//  Created by csuftitan on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct cpsc_411_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
